4cca5c67e6892abad63705ac0f5778f82881c963
azizkale <azizkale@hotmail.com>
2022-01-29 18:01:48 +0100

    initial

---
+++ Assets/Scripts/Memory.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class Memory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        createTxtFile();
+    }
+
+    void createTxtFile()
+    {
+        string path = Application.persistentDataPath + "/words.txt";
+        if (!File.Exists(path))
+        {
+            // creates the txt file
+            File.WriteAllText(path, "");
+        }
+        // add line to txt file
+        File.AppendAllText(path, "aiz kale \n");
+
+        string[] words = File.ReadAllLines(path);
+
+        foreach (string item in words)
+        {
+            Debug.Log(item.ToString());
+        }
+    }
+}
---
+++ Assets/Scripts/Turning.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class Turning : MonoBehaviour
+{
+    public GameObject appCube;
+    string m_Path;
+    void Start()
+    {
+     
+
+    }
+
+    
+
+   IEnumerator turnTheCard(GameObject go)
+    {
+        if (go.transform.rotation == Quaternion.Euler(0, 0, 0))
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                //go.transform.Rotate(new Vector3(0, i*18, 0));
+                go.transform.rotation = Quaternion.Euler(0, 180, 0);
+                yield return new WaitForSeconds(0.00001f);
+            }
+        }
+
+        if (go.transform.rotation == Quaternion.Euler(0, 180, 0))
+        {
+            //for (int i = 0; i <= 10; i--)
+            //{
+                //go.transform.Rotate(new Vector3(0, i*18, 0));
+                go.transform.rotation = Quaternion.Euler(0, 0, 0);
+                yield return new WaitForSeconds(0.00001f);
+            //}
+        }
+    }
+   
+
+   
+    public void turnToBack()
+    {
+       appCube.transform.rotation = Quaternion.Euler(0, 180, 0);
+    }
+    public void turnToFront()
+    {
+        appCube.transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+}
