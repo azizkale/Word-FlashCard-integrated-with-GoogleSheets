ba28db9fd624ad4a34c55891861134d0769c8f16
devmath <devmath@yandex.com>
2022-02-22 16:39:26 +0100

    .

--- Assets/Scripts/MyLibrary/MyLibrary.cs
+++ Assets/Scripts/MyLibrary/MyLibrary.cs
@@ -1,18 +1,21 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MyLibrary : MonoBehaviour
 {
-    public GameObject wordsPairCard; //prefab
+    public GameObject prefabWordsPairCard; 
+    public GameObject prefabUpdateWord;
+    public GameObject prefabCompleteSuccessfully;
+    public GameObject prefabGeneralWarnung;
     public GameObject scrolContainer;
-    GameObject clone;
+    public GameObject canvas;
 
     TextMeshProUGUI cardQuestion;
     TextMeshProUGUI cardAnswer;
-    public TextMeshProUGUI sceneTitle;
+    //public TextMeshProUGUI sceneTitle;
+    private Library theLibrary;
     void Start()
     {
         createFileNamesCards();
@@ -21,17 +24,22 @@ public class MyLibrary : MonoBehaviour
     public void createFileNamesCards()
     {
 
-        Library theLibrary = Read.getLibraryContentWithoutArchive(CommonVariables.libraryName);
+        theLibrary = Read.getLibraryContentWithoutArchive(CommonVariables.libraryName);
             //JsonConvert.DeserializeObject<Library>(PlayerPrefs.GetString(CommonVariables.libraryName));
 
         foreach (Word word in theLibrary.words)
         {
-            clone = Instantiate(wordsPairCard, scrolContainer.transform.position, Quaternion.identity, scrolContainer.transform) as GameObject;
+           GameObject clone = Instantiate(prefabWordsPairCard, scrolContainer.transform.position, Quaternion.identity, scrolContainer.transform) as GameObject;
 
             cardQuestion = clone.transform.Find("Button").transform.Find("TMP_question").GetComponent<TextMeshProUGUI>();
             cardQuestion.text = CommonVariables.charachterLimit(word.theWord,30);
+
             cardAnswer = clone.transform.Find("Button").transform.Find("TMP_answer").GetComponent<TextMeshProUGUI>();
             cardAnswer.text = CommonVariables.charachterLimit(word.meaning,30);
+
+            clone.transform.Find("Button").GetComponent<Button>().onClick.AddListener(() => {
+                updateTheWord(word, canvas);
+            });
         }
     }
 
@@ -43,5 +51,41 @@ public class MyLibrary : MonoBehaviour
     public void startLearning()
     {
 
+    }
+
+    private void updateTheWord(Word word, GameObject canvas)
+    {
+        GameObject cloneprefabupdateword = Instantiate(prefabUpdateWord, canvas.transform.position, Quaternion.identity, canvas.transform);
+        cloneprefabupdateword.transform.localPosition = Vector3.zero;
+
+        //question text
+        TMP_InputField inputQ = cloneprefabupdateword.transform.Find("TMPQuestion").GetComponent<TMP_InputField>();
+        inputQ.text = word.theWord;
+
+        //answer text
+        TMP_InputField inputA = cloneprefabupdateword.transform.Find("TMPAnswer").GetComponent<TMP_InputField>();
+        inputA.text = word.meaning;
+
+        //when this is clicked, the text of TMP_InputFileds are updated
+        cloneprefabupdateword.transform.Find("btnUpdate").GetComponent<Button>().onClick.AddListener(() => {
+            if (!string.IsNullOrEmpty(inputQ.text) && !string.IsNullOrEmpty(inputA.text))
+            {
+                Update.updateSingleWord(word, theLibrary, inputQ.text, inputA.text);
+                //chamges the displayWord on the screen
+                word.theWord = inputQ.text;
+                word.meaning = inputA.text;              
+
+                DestroyImmediate(cloneprefabupdateword);
+                alertWarning.completedSuccesfully(prefabCompleteSuccessfully, canvas);
+
+
+            }
+            else
+                alertWarning.generalWarning(prefabGeneralWarnung, canvas, "Question and answer fields cannot be left blank!");
+        });
+
+        cloneprefabupdateword.transform.Find("btnCancel").GetComponent<Button>().onClick.AddListener(() => {
+            DestroyImmediate(cloneprefabupdateword);
+        });
     }
 }
